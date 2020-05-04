8115c8ffc92de1219105abab5004b240eab0e6df
HonzaLed <43474035+HonzaLed@users.noreply.github.com>
2020-05-04 16:17:28 +0200

    Update hackumimecesky.js

--- hackumimecesky.js
+++ hackumimecesky.js
@@ -1,8 +1,9 @@
-window.setInterval('function(){
+function pressCorrect(){
 if (questions[questionOffset].options[1].correct == "0"){
 $(".option0").trigger("click");
 }
 else{
 $(".option1").trigger("click");
-}}', 2000)
+}}
+window.setInterval(pressCorrect(), 2000)
 
