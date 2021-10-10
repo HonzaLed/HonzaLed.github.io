f6fbaada728e8fd028f9e841ebf13bdfbf30692b
HonzaLed <43474035+HonzaLed@users.noreply.github.com>
2021-10-10 12:10:41 +0200

    Delete hackumimecesky.js

--- hackumimecesky.js
+++
@@ -1,9 +0,0 @@
-function pressCorrect(){
-if (questions[questionOffset].options[1].correct == "0"){
-$(".option0").trigger("click");
-}
-else{
-$(".option1").trigger("click");
-}}
-a = window.setInterval(pressCorrect(), 2000)
-
